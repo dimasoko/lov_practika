dfc91756999fd1acc863dac5b03cd2c960027d02
dmitriy <dimasokolov28022006@gmail.com>
2025-05-20 03:53:36 +0700

    ROT13

---
+++ ROT13/script.js
@@ -0,0 +1,58 @@
+const inputText = document.getElementById('inputText');
+const outputText = document.getElementById('outputText');
+
+const lowAlphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upAlphabetArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+let showAlphabet = '';
+for (let i = 0; i < lowAlphabetArray.length; i++) {
+    showAlphabet += lowAlphabetArray[i];
+}
+showAlphabet += ' ';
+for (let i = 0; i < upAlphabetArray.length; i++) {
+    showAlphabet += upAlphabetArray[i];
+}
+
+const ROT_SHIFT = 13;
+const ALPHABET_LENGTH = 26;
+
+function getTransformedCharIfFound(charToTransform, alphabetArray) {
+    for (let i = 0; i < alphabetArray.length; i++) {
+        if (charToTransform === alphabetArray[i]) {
+            const newIndex = (i + ROT_SHIFT) % ALPHABET_LENGTH; // а расшифровка и шифровка то одинаковые!
+            const transformed = alphabetArray[newIndex];
+            const logEntry = `${charToTransform} - 13 = ${transformed}`;
+            return { char: transformed, log: logEntry }; // Объект без found: true, если найдено
+        }
+    }
+    return null; // null, если не найдено в этом алфавите
+}
+
+function transformCharWithROT13(char) {
+    const result = getTransformedCharIfFound(char, lowAlphabetArray) || 
+                   getTransformedCharIfFound(char, upAlphabetArray);
+
+    return result ? 
+        { char: result.char, log: result.log } : 
+        { char: char, log: `'${char}' (не расшифровывается)` };
+}
+
+function decryptTextAndLogDetails(text) {
+    let resultText = '';
+    let logDetails = `\nалфавит: ${showAlphabet}\nпроцесс:\n`;
+
+    for (let i = 0; i < text.length; i++) {
+        const originalChar = text[i];
+        const decryption = transformCharWithROT13(originalChar);
+        resultText += decryption.char;
+        if (decryption.log) { 
+            logDetails += decryption.log + '\n';
+        }
+    }
+    return resultText + logDetails;
+}
+
+inputText.addEventListener('input', () => {
+    const textToProcess = inputText.value;
+    outputText.value = decryptTextAndLogDetails(textToProcess);
+});
